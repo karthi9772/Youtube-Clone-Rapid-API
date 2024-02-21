b07f61c070ccad13a0520a58198f1c41d80aad9e
karthi9772 <karthi.g9772@gmail.com>
2024-02-22 02:59:49 +0530

    Added The VideoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,7 +19,13 @@ const VideoCard = ({
 }) => {
   console.log(videoId, snippet);
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: "320px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: "0",
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
